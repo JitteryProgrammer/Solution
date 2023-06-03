d14d02473826b5aaef06ab0454856332632d6042
JitteryProgrammer <120346064+JitteryProgrammer@users.noreply.github.com>
2023-06-03 15:49:33 -0300

    Update Solution.py

--- Solution.py
+++ Solution.py
@@ -20,21 +20,42 @@ class Solution:
                 right -= 1
         return maxi
 
+    def calculateTotalCapacity(self, height: list[int]) -> float:
+        if not height or not isinstance(height, list):
+            raise ValueError("A lista de alturas fornecida é inválida.")
+
+        s = Solution()
+        max_capacity = s.maxArea(height)
+        total_capacity = max_capacity * 100
+        return total_capacity
+
+    def plotTerrainHeight(self, height: list[int]) -> None:
+        if not height or not isinstance(height, list):
+            raise ValueError("A lista de alturas fornecida é inválida.")
+
+        plt.bar(np.arange(len(height)), height, color='blue')
+        plt.title("Altura do terreno")
+        plt.xlabel("Posição")
+        plt.ylabel("Altura (m)")
+        plt.show()
+
+
 # Altura do terreno em metros
 height = np.array([10, 15, 20, 25, 30, 25, 20, 15, 10, 5, 10, 15, 20, 25, 30, 25, 20, 15, 10, 5])
 
-# Estimativa da capacidade de armazenamento de água
+# Criando uma instância da classe Solution
 s = Solution()
-max_capacity = s.maxArea(height)
 
-# Capacidade total em metros cúbicos
-total_capacity = max_capacity * 100
-print("A capacidade máxima de armazenamento de água do reservatório é de:", total_capacity, "metros cúbicos")
+# Calculando a capacidade total de armazenamento de água
+try:
+    total_capacity = s.calculateTotalCapacity(height)
+    print("A capacidade máxima de armazenamento de água do reservatório é de:", total_capacity, "metros cúbicos")
+except ValueError as e:
+    print("Erro:", str(e))
 
-#Crie um gráfico de barras para visualizar a altura do terreno
+# Criando o gráfico de barras para visualizar a altura do terreno
+try:
+    s.plotTerrainHeight(height)
+except ValueError as e:
+    print("Erro:", str(e))
 
-plt.bar(np.arange(len(height)), height, color = 'blue')
-plt.title("Altura do terreno")
-plt.xlabel("Posição")
-plt.ylabel("Altura (m)")
-plt.show()
