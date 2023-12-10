8ce4755d39c9eb1278fdcb1be0cf358fd0aa495a
João Vitor <120346064+JitteryProgrammer@users.noreply.github.com>
2023-12-10 01:59:05 -0300

    Update Solution.py

--- Solution.py
+++ Solution.py
@@ -3,6 +3,15 @@ import matplotlib.pyplot as plt
 
 class Solution:
     def maxArea(self, height: list[int]) -> int:
+        """
+        Calcula a área máxima de um contêiner retangular formado pelas alturas fornecidas.
+
+        Parameters:
+            height (List[int]): Lista de alturas.
+
+        Returns:
+            int: Área máxima do contêiner.
+        """
         left = 0
         right = len(height) - 1
         maxi = 0
@@ -21,39 +30,53 @@ class Solution:
         return maxi
 
     def calculateTotalCapacity(self, height: list[int]) -> float:
+        """
+        Calcula a capacidade total de armazenamento de água com base nas alturas fornecidas.
+
+        Parameters:
+            height (List[int]): Lista de alturas.
+
+        Returns:
+            float: Capacidade total de armazenamento de água em metros cúbicos.
+        """
         if not height or not isinstance(height, list):
             raise ValueError("A lista de alturas fornecida é inválida.")
 
         s = Solution()
         max_capacity = s.maxArea(height)
-        total_capacity = max_capacity * 100
+        total_capacity = max_capacity * 100  # Ajuste para metros cúbicos
         return total_capacity
 
     def plotTerrainHeight(self, height: list[int]) -> None:
+        """
+        Gera um gráfico de barras para visualizar a altura do terreno.
+
+        Parameters:
+            height (List[int]): Lista de alturas.
+
+        Returns:
+            None
+        """
         if not height or not isinstance(height, list):
             raise ValueError("A lista de alturas fornecida é inválida.")
 
         plt.bar(np.arange(len(height)), height, color='blue')
-        plt.title("Altura do terreno")
+        plt.title(f"Altura do Terreno - Capacidade: {self.calculateTotalCapacity(height):.2f} metros cúbicos")
         plt.xlabel("Posição")
         plt.ylabel("Altura (m)")
         plt.show()
 
+user_height = input("Digite a altura do terreno em metros (separe os valores por espaço): ")
+height = [int(h) for h in user_height.split()]
 
-# Altura do terreno em metros
-height = np.array([10, 15, 20, 25, 30, 25, 20, 15, 10, 5, 10, 15, 20, 25, 30, 25, 20, 15, 10, 5])
-
-# Criando uma instância da classe Solution
 s = Solution()
 
-# Calculando a capacidade total de armazenamento de água
 try:
     total_capacity = s.calculateTotalCapacity(height)
     print("A capacidade máxima de armazenamento de água do reservatório é de:", total_capacity, "metros cúbicos")
 except ValueError as e:
     print("Erro:", str(e))
 
-# Criando o gráfico de barras para visualizar a altura do terreno
 try:
     s.plotTerrainHeight(height)
 except ValueError as e:
